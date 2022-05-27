3ac4bea63527a8865136cbc6e5503dda12ac99c5
s0163475 <90443229+s0163475@users.noreply.github.com>
2022-05-27 10:39:17 +0300

    Update update.php

--- main/layout/header/update.php
+++ main/layout/header/update.php
@@ -1,6 +1,6 @@
 <div class="form">
 	<div class="form__header">
 		<div class='form__container form__container_good'>
-			<span class='form__span'>Ваши данные обновленны!</span>
+			<span class='form__span'>Данные обновленны!</span>
 		</div>
 	</div>
