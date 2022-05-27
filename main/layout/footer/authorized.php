4c7a43dc2787d4b2ff651950227fa5f9623e7451
s0163475 <90443229+s0163475@users.noreply.github.com>
2022-05-27 10:05:38 +0300

    Update authorized.php

--- main/layout/footer/authorized.php
+++ main/layout/footer/authorized.php
@@ -3,7 +3,7 @@
 		Вы авторизованы, ваш логин: <strong><?php echo $_SESSION['login'] ?></strong>
 	</p>
 	<p class='form__p'>
-		Вы можете <a href='login.php' class='form__a'>выйти</a>!
+		Желаете <a href='login.php' class='form__a'>выйти</a>?
 	</p>
 </div>
 </div>
