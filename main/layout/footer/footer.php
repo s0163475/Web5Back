b0bffc8e5ab777c632f9eb7481141f1e3a0b3d57
s0163475 <90443229+s0163475@users.noreply.github.com>
2022-05-27 09:53:11 +0300

    Add files via upload

---
+++ main/layout/footer/footer.php
@@ -0,0 +1,9 @@
+<div class='form__footer'>
+	<p class='form__p'>
+		У вас уже есть аккаунт?
+	</p>
+	<p class='form__p'>
+		Вы можете <a href='login.php' class='form__a'>войти</a>!
+	</p>
+</div>
+</div>
---
+++ src/db.php
@@ -0,0 +1,5 @@
+<?php
+$dbServerName = 'localhost';
+$dbUser = "u47543";
+$dbPassword = "3562299";
+$dbName = $dbUser;
