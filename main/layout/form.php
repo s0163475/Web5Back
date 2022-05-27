a7683c50f63d3cc9643ed337cd9e98399518cf8e
s0163475 <90443229+s0163475@users.noreply.github.com>
2022-05-27 10:12:01 +0300

    Update form.php

--- main/layout/form.php
+++ main/layout/form.php
@@ -1,20 +1,20 @@
 <form class="form__body" action="" method="post">
 	<div class="form__item">
 		<label class="form__label">
-			<input class="form__input form__input_text" placeholder="Имя" type="text" name="name" value="<?php echo $message['name']; ?>">
+			<input class="form__input form__input_text" placeholder="Ваше имя" type="text" name="name" value="<?php echo $message['name']; ?>">
 		</label>
 		<?php echo $message['name-error']; ?>
 	</div>
 	<div class="form__item">
 		<label class="form__label">
-			<input class="form__input form__input_text" placeholder="E-mail" type="text" name="email" value="<?php echo $message['email']; ?>">
+			<input class="form__input form__input_text" placeholder="Ваш e-mail" type="text" name="email" value="<?php echo $message['email']; ?>">
 		</label>
 		<?php echo $message['email-error']; ?>
 	</div>
 	<div class="form__item">
 		<label class="form__label">
 			<select name="year" class="form__select">
-				<option value="" class="form__option">Год</option>
+				<option value="" class="form__option">Ваш год рождения</option>
 				<?php
 				for ($i = 2008; $i >= 1900; --$i) {
 					if ($i == $message['year']) {
@@ -52,7 +52,7 @@
 		<?php echo $message['gender-error']; ?>
 	</div>
 	<div class="form__item form__item_numlimbs">
-		<span class="form__span">Количество конечностей</span>
+		<span class="form__span">Кол-во ваших конечностей</span>
 		<div class="form__container">
 			<?php
 			for ($i = 1; $i <= 4; $i++) {
@@ -68,7 +68,7 @@
 	</div>
 	<div class="form__item form__item_sp">
 		<label class="form__label">
-			<span class="form__span">Сверхспособности</span>
+			<span class="form__span">Ваши сверхспособности</span>
 			<select multiple name="super-powers[]" class="form__select">
 				<option class="form__oprion" <?php echo ((isset($message["super-powers"]['1']) && $message["super-powers"]['1'] == '1') ? 'selected' : ''); ?> value="1">Бессмертие</option>
 				<option class="form__oprion" <?php echo ((isset($message["super-powers"]['2']) && $message["super-powers"]['2'] == '1') ? 'selected' : ''); ?> value="2">Прохождение сквозь стены</option>
@@ -79,18 +79,18 @@
 	</div>
 	<div class="form__item">
 		<label class="form__label">
-			<textarea class="form__textarea" placeholder="Расскажите о себе" name="biography"><?php echo $message['biography']; ?></textarea>
+			<textarea class="form__textarea" placeholder="О вас" name="biography"><?php echo $message['biography']; ?></textarea>
 		</label>
 		<?php echo $message['biography-error'] ?>
 	</div>
 	<div class="form__item form__item_agreement">
 		<label class="form__label">
-			<input class="form__input" type="checkbox" name="agree" required>Согласие на обработку данных
+			<input class="form__input" type="checkbox" name="agree" required>Согласие на обработку персональных данных
 		</label>
 	</div>
 	<div class="form__item form__item_submit">
 		<label class="form__label">
-			<input class="form__submit" type="submit" value="Отправить">
+			<input class="form__submit" type="submit" value="Отправить!">
 		</label>
 	</div>
 </form>
