132d54970e0f944d66883bd2f41088f428ac0140
s0163475 <90443229+s0163475@users.noreply.github.com>
2022-05-27 10:14:52 +0300

    Update header.php

--- main/layout/header/header.php
+++ main/layout/header/header.php
@@ -1,6 +1,6 @@
 <div class="form">
 	<div class="form__header">
 		<div class='form__contaner'>
-			<span class='form__span form__span_header'>ЗАПОЛНИТЕ</span>
+			<span class='form__span form__span_header'>Заполните, пожалуйста!</span>
 		</div>
 	</div>
