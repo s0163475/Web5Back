d14f7762f27cd40c3dc27b9dd3789fec1c4521cb
s0163475 <90443229+s0163475@users.noreply.github.com>
2022-05-27 10:14:10 +0300

    Update error.php

--- main/layout/header/error.php
+++ main/layout/header/error.php
@@ -1,6 +1,6 @@
 <div class="form">
 	<div class="form__header">
 		<div class='form__container form__container_err'>
-			<span class='form__span'>Что-то пошло не так! =(</span>
+			<span class='form__span'>Ой-ой, похоже, что-то пошло не так! =(</span>
 		</div>
 	</div>
