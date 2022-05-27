691f9cf3f3d97f0eedd5130d3cb47cfcd97e1ed2
s0163475 <90443229+s0163475@users.noreply.github.com>
2022-05-27 10:09:17 +0300

    Update formHandler.php

--- src/formHandler.php
+++ src/formHandler.php
@@ -9,35 +9,35 @@ class formHandler
 		if (empty($user->getName())) {
 			$errors['name'] = "Введите имя!";
 		} elseif (!preg_match("/^\s*[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ]+[a-zA-Zа-яА-Я']?\s*$/u", $user->getName())) {
-			$errors['name'] = "Несуществующее имя!";
+			$errors['name'] = "Не корректно указано имя!";
 		}
 
 		if (empty($user->getEmail())) {
 			$errors['email'] = "Введите e-mail!";
 		} elseif (!preg_match("/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/", $user->getEmail())) {
-			$errors['email'] = "Несуществующий e-mail!";
+			$errors['email'] = "Не корректно указан e-mail!";
 		}
 
 		if (empty($user->getYear())) {
-			$errors['year'] = "Выберите год!";
+			$errors['year'] = "Пожалуйста, выберите год вышего рождения!";
 		} elseif (!preg_match("/^\s*[1]{1}9{1}\d{1}\d{1}.*$|^\s*200[0-8]{1}.*$/", $user->getYear())) {
 			$errors["requestError"] = true;
 		}
 
 		if (empty($user->getGender())) {
-			$errors['gender'] = "Выберите пол!";
+			$errors['gender'] = "Пожалуйста, выберите ваш пол!";
 		} elseif (intval($user->getGender()) < 1 && 2 < intval($user->getGender())) {
 			$errors["requestError"] = true;
 		}
 
 		if (empty($user->getNumlimbs())) {
-			$errors['numlimbs'] = "Выберите кол-во конечностей!";
+			$errors['numlimbs'] = "Пожалуйста, выберите кол-во ваших конечностей!";
 		} elseif (intval($user->getNumlimbs()) < 1 || 4 < intval($user->getNumlimbs())) {
 			$errors["requestError"] = true;
 		}
 
 		if (empty($user->getSuperPowers())) {
-			$errors['super-powers'] = "Выберите хотя бы одну суперспособность!";
+			$errors['super-powers'] = "Пожалуйста выберите ваши суперспособности!";
 		} else {
 			foreach ($user->getSuperPowers() as $value) {
 				if (intval($value) < 1 || 3 < intval($value)) {
@@ -48,7 +48,7 @@ class formHandler
 		}
 
 		if (empty($user->getBiography())) {
-			$errors['biography'] = "Расскажите что-нибудь о себе!";
+			$errors['biography'] = "Пожалуйста, расскажите что-нибудь о себе!";
 		}
 
 		return $errors;
