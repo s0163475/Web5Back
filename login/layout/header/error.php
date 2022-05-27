b559b80d1a3b6521272ec54d73d39028e6672e87
s0163475 <90443229+s0163475@users.noreply.github.com>
2022-05-27 10:58:42 +0300

    Delete login directory

--- login/layout/header/error.php
+++
@@ -1,5 +0,0 @@
-<div class='form__header'>
-	<div class='form__contaner form__container_err'>
-		<span class='form__span'>Что-то пошло не так! =(</span>
-	</div>
-</div>
--- login/layout/header/header.php
+++
@@ -1,5 +0,0 @@
-<div class='form__header'>
-	<div class='form__contaner'>
-		<span class='form__span form__span_header'>Авторизируйтесь</span>
-	</div>
-</div>
